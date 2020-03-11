179a82e3b87632cf1405505dc90aa6ae8786b114
Szigeti Péter <szigeti.peter23@gmail.com>
2020-03-11 18:32:19 +0100

    Missed during previous commit

---
+++ include/controls.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "control_common.hpp"
+#include "point.hpp"
+#include "rect.hpp"
+#include "control.hpp"
+#include "frame.hpp"
+#include "label.hpp"
+#include "spinner.hpp"
+#include "button.hpp"
+#include "textbox.hpp"
+#include "scene.hpp"
